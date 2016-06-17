3b37b499fea2c85f31300a704c9b61df442196d0
Donald Lessells <donald.lessells@gmail.com>
2016-06-17 21:31:29 +0100

    added board array to gameboard. added some tests and functionality for gameboard

--- client/src/models/gameboard.js
+++ client/src/models/gameboard.js
@@ -1,8 +1,13 @@
 var GameBoard = function() {
-    this.board = new Array(20);
-    for (var i = 0; i < 20; i++) {
-      this.board[i] = new Array(20);
-    }
+    // this.board = new Array(20);
+    // for (var i = 0; i < 20; i++) {
+    //   this.board[i] = new Array(20);
+    // }
+    this.boardArray = this.generateBoardArray();
+    this.redCount = 0;
+    this.greenCount = 0;
+    this.blueCount = 0;
+    this.yellowCount = 0;
 };
 
 GameBoard.prototype = {
@@ -10,6 +15,27 @@ GameBoard.prototype = {
         console.log(piece.array);
         var x = pivotPoint[0];
         var y = pivotPoint[1];
+    },
+    generateBoardArray: function(){
+      var array = new Array(20);
+      for (var i = 0; i < 20; i++) {
+        array[i] = new Array(20);
+        for (var j = 0; j < 20; j++) {
+            array[i][j] = [];
+        }
+      }
+      return array;
+    },
+    fill: function(coordinates, colourString) {
+      this.boardArray[coordinates[0]][coordinates[1]].push(colourString);
+    },
+    inBounds: function(coordinates) {
+      if (this.boardArray[coordinates[0]] && this.boardArray[coordinates[0]][coordinates[1]]) {
+        return true;
+      }
+        else {
+          return false;
+        }
     }
 };
 
---
+++ client/src/models/specs/gameboard_specs.js
@@ -0,0 +1,59 @@
+var assert = require('chai').assert;
+var GameBoard = require('../gameboard.js');
+
+describe('GameBoard', function() {
+    beforeEach(function(){
+        
+        board = new GameBoard();
+
+    });
+    it('Its board array is an array of twenty arrays of twenty empty arrays!', function() {
+
+        assert.equal(20, board.boardArray.length);
+        assert.equal(20, board.boardArray[0].length);
+        assert.equal(0, board.boardArray[0][0].length);
+    });
+    it('can colour a square when provided with a colour and coordinates', function() {
+
+        board.fill([9,7], 'R');
+        console.log(board.boardArray[9][7][0]);
+        assert.equal('R', board.boardArray[9][7][0]);
+    });
+    it('can determine if a given set of coordinates falls within the bounds of its board array', function() {
+
+        assert.equal(true, board.inBounds([10,10]));
+        assert.equal(false, board.inBounds([20,20]));
+    });
+    it('can decide which corners of a placed piece are in bounds', function() {
+
+    });
+    it('knows if a piece has been placed where one or more of its tiles fall out of bounds', function() {
+
+    });
+    it('can decide which adjacent squares of a placed piece are in bounds', function() {
+
+    });
+    it('can tell when two placed pieces are overlapping at a given square', function() {
+
+    });
+    it('can tell when a placed piece is overlapping one or more occupied squares', function() {
+
+    });
+    it('can decide if a piece placement meets the corner requirement', function() {
+
+    });
+    it('can decide if a piece placement meets the edge requirement', function() {
+
+    });
+
+});
+
+
+
+
+
+
+
+
+
+
